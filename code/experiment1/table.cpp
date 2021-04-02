534ecd5a63576bf07f691853fbe2ea7b0b36a833
Aj0SK <andrejkorman@gmail.com>
2021-04-02 23:09:46 +0200

    Simple table lookup algorithm

--- code/experiment1/table.cpp
+++ code/experiment1/table.cpp
@@ -9,7 +9,25 @@ using std::vector;
 constexpr size_t kN = 1 << 3;
 constexpr size_t kC = 4;
 
-vector<vector<int>> create_helper(size_t n, size_t c)
+constexpr size_t nCr(size_t N, size_t K)
+{
+  // This function gets the total number of unique combinations based upon N and
+  // K. N is the total number of items. K is the size of the group. Total number
+  // of unique combinations = N! / ( K! (N - K)! ). This function is less
+  // efficient, but is more likely to not overflow when N and K are large. Taken
+  // from:  http://blog.plover.com/math/choose.html
+  if (K > N)
+    return 0;
+  size_t r = 1;
+  for (size_t d = 1; d <= K; ++d)
+  {
+    r *= N--;
+    r /= d;
+  }
+  return r;
+}
+
+vector<vector<bool>> create_helper(size_t n, size_t c)
 {
   if (c >= n)
   {
@@ -17,7 +35,7 @@ vector<vector<int>> create_helper(size_t n, size_t c)
          << "\n";
     exit(0);
   }
-  vector<int> arr;
+  vector<bool> arr;
   arr.reserve(n);
 
   for (size_t i = 0; i < n - c; ++i)
@@ -25,7 +43,7 @@ vector<vector<int>> create_helper(size_t n, size_t c)
   for (size_t i = 0; i < c; ++i)
     arr.push_back(1);
 
-  vector<vector<int>> out;
+  vector<vector<bool>> out;
   do
   {
     out.push_back(arr);
@@ -34,16 +52,50 @@ vector<vector<int>> create_helper(size_t n, size_t c)
   return out;
 }
 
+vector<bool> get_ith(size_t n, size_t c, size_t target)
+{
+  vector<bool> out;
+  for (size_t i = 0; i < n; ++i)
+  {
+    size_t bits_to_right = n - i - 1;
+    size_t x = nCr(bits_to_right, c);
+    if (target > x)
+    {
+      --c;
+      out.push_back(1);
+      target -= x;
+    }
+    else
+    {
+      out.push_back(0);
+    }
+  }
+
+  return out;
+}
+
 int main()
 {
-  auto helper = create_helper(kN, kC);
-  for (auto& arr : helper)
   {
-    for (auto x : arr)
+    // some tests
+    static_assert(nCr(12, 4) == 495);
+    static_assert(nCr(45, 10) == 3'190'187'286);
+    static_assert(nCr(30, 3) == 4'060);
+    static_assert(nCr(25, 7) == 480'700);
+    size_t kTestN = 13;
+    size_t kTestC = 3;
+
+    auto helper = create_helper(kTestN, kTestC);
+    for (size_t i = 0; i < helper.size(); ++i)
     {
-      cout << x;
+      if (helper[i] != get_ith(kTestN, kTestC, i + 1))
+      {
+        cout << "Error in get_ith"
+             << "\n";
+        return 0;
+      }
     }
-    cout << "\n";
   }
+
   return 0;
 }
