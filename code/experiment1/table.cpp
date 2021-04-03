5822a59b43b993abd8753f7bd29c7ef45a71499c
Aj0SK <andrejkorman@gmail.com>
2021-04-03 10:26:16 +0200

    Add faster sequence finder

--- code/experiment1/table.cpp
+++ code/experiment1/table.cpp
@@ -13,12 +13,15 @@ constexpr size_t kN = 1 << 3;
 constexpr size_t kC = 4;
 constexpr size_t kMaxBinN = 64;
 
-// from https://cp-algorithms.com/combinatorics/binomial-coefficients.html
+constexpr size_t kTestN = 13;
+constexpr size_t kTestC = 3;
+
 class BinCoeff
 {
 public:
   using Arr = std::array<std::array<std::size_t, kMaxBinN + 1>, kMaxBinN + 1>;
 
+  // from: https://cp-algorithms.com/combinatorics/binomial-coefficients.html
   static constexpr Arr set_data()
   {
     Arr C = {};
@@ -35,15 +38,14 @@ public:
   }
 };
 
+// stores C(n, k) for all pairs of n and k and should be computed at the
+// compile time
 static constexpr BinCoeff::Arr nCrArr{BinCoeff::set_data()};
 
+// computes and returns C(n, k)
 constexpr size_t nCr(size_t N, size_t K)
 {
-  // This function gets the total number of unique combinations based upon N and
-  // K. N is the total number of items. K is the size of the group. Total number
-  // of unique combinations = N! / ( K! (N - K)! ). This function is less
-  // efficient, but is more likely to not overflow when N and K are large. Taken
-  // from:  http://blog.plover.com/math/choose.html
+  // from: http://blog.plover.com/math/choose.html
   if (K > N)
     return 0;
   size_t r = 1;
@@ -55,11 +57,13 @@ constexpr size_t nCr(size_t N, size_t K)
   return r;
 }
 
-vector<vector<bool>> create_helper(size_t n, size_t c)
+// returns vector of all n-bit sequences with c-bits set
+// in the lexigraphical order
+vector<vector<bool>> sorted_bit_sequences(size_t n, size_t c)
 {
-  if (c >= n)
+  if (c > n)
   {
-    cerr << "Bad c in create_helper."
+    cerr << "Bad c in sorted_bit_sequences."
          << "\n";
     exit(0);
   }
@@ -83,6 +87,7 @@ vector<vector<bool>> create_helper(size_t n, size_t c)
 vector<bool> get_ith(size_t n, size_t c, size_t target)
 {
   vector<bool> out;
+  out.reserve(n);
   for (size_t i = 0; i < n; ++i)
   {
     size_t bits_to_right = n - i - 1;
@@ -103,7 +108,26 @@ vector<bool> get_ith(size_t n, size_t c, size_t target)
   return out;
 }
 
-static void BM_SomeFunction1(benchmark::State& state)
+size_t get_ith_fast(size_t n, size_t c, size_t target)
+{
+  size_t out;
+  for (size_t i = 0; i < n; ++i)
+  {
+    size_t bits_to_right = n - i - 1;
+    size_t count_with_zero = nCrArr[bits_to_right][c];
+    out <<= 1;
+    if (target > count_with_zero)
+    {
+      --c;
+      out |= 1;
+      target -= count_with_zero;
+    }
+  }
+
+  return out;
+}
+
+static void BM_BinCoeffPrecomputed(benchmark::State& state)
 {
   srand(0);
   for (auto _ : state)
@@ -115,7 +139,7 @@ static void BM_SomeFunction1(benchmark::State& state)
   }
 }
 
-static void BM_SomeFunction2(benchmark::State& state)
+static void BM_BinCoeffCompute(benchmark::State& state)
 {
   srand(0);
   for (auto _ : state)
@@ -127,8 +151,42 @@ static void BM_SomeFunction2(benchmark::State& state)
   }
 }
 
-BENCHMARK(BM_SomeFunction1);
-BENCHMARK(BM_SomeFunction2);
+BENCHMARK(BM_BinCoeffPrecomputed);
+BENCHMARK(BM_BinCoeffCompute);
+
+static void BM_GetIth(benchmark::State& state)
+{
+  srand(0);
+  size_t a = kMaxBinN;
+  for (auto _ : state)
+  {
+    state.PauseTiming();
+    size_t b = 1 + rand() % a;
+    size_t c = 1 + rand() % nCrArr[a][b];
+    state.ResumeTiming();
+    auto out = get_ith(a, b, c);
+    benchmark::DoNotOptimize(out);
+  }
+}
+
+BENCHMARK(BM_GetIth);
+
+static void BM_GetIthFast(benchmark::State& state)
+{
+  srand(0);
+  size_t a = kMaxBinN;
+  for (auto _ : state)
+  {
+    state.PauseTiming();
+    size_t b = 1 + rand() % a;
+    size_t c = 1 + rand() % nCrArr[a][b];
+    state.ResumeTiming();
+    auto out = get_ith_fast(a, b, c);
+    benchmark::DoNotOptimize(out);
+  }
+}
+
+BENCHMARK(BM_GetIthFast);
 
 int main(int argc, char** argv)
 {
@@ -149,13 +207,20 @@ int main(int argc, char** argv)
     static_assert(nCrArr[45][10] == 3'190'187'286);
     static_assert(nCrArr[25][7] == 480'700);
 
-    size_t kTestN = 13;
-    size_t kTestC = 3;
-
-    auto helper = create_helper(kTestN, kTestC);
+    auto helper = sorted_bit_sequences(kTestN, kTestC);
     for (size_t i = 0; i < helper.size(); ++i)
     {
-      if (helper[i] != get_ith(kTestN, kTestC, i + 1))
+      auto index = get_ith_fast(kTestN, kTestC, i + 1);
+      vector<bool> fast_out;
+      for (size_t i = 0; i < kTestN; ++i)
+      {
+        size_t set_bit = 1 << (kTestN - i - 1);
+        fast_out.push_back(index & set_bit);
+      }
+
+      if (auto real_result = helper[i];
+          real_result != get_ith(kTestN, kTestC, i + 1) ||
+          real_result != fast_out)
       {
         cout << "Error in get_ith"
              << "\n";
@@ -172,6 +237,9 @@ int main(int argc, char** argv)
           return 0;
         }
       }
+
+    cout << "Successfully run some basic tests!"
+         << "\n";
   }
 
   return 0;
