d72c4238ce829c5f82b9a4727ff833222a963b79
Andrej Korman <andrejkorman@gmail.com>
2021-12-24 12:19:49 +0100

    Add locate benchmark

---
+++ code/experiment4/benchmark_sdsl_indexing_locate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+REPOSRC=https://github.com/Aj0SK/sdsl-lite.git
+
+rm -f res1-locate.txt res2-locate.txt
+git clone "$REPOSRC" sdsl-old 2> /dev/null || (cd "sdsl-old" ; git pull ; cd ..)
+cd sdsl-old/build
+
+git checkout benchmark_original
+./build.sh
+cd ../benchmark/indexing_locate
+make clean-build
+make cleanresults
+make timing
+cp ./results/all.txt ../../../res1-locate.txt
+
+cd ../../../
+git clone "$REPOSRC" sdsl-new 2> /dev/null || (cd "sdsl-new" ; git pull ; cd ..)
+cd sdsl-new/build
+
+git checkout refactor
+./build.sh
+cd ../benchmark/indexing_locate
+make clean-build
+make cleanresults
+make timing
+cp ./results/all.txt ../../../res2-locate.txt
