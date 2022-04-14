964c9627fc11670725071da2d818344c266ec5c1
Andrej Korman <andrejkorman@gmail.com>
2022-04-15 01:13:25 +0200

    Code: changes to chapter 3 and 4

--- code/experiment3/rrr_savings.py
+++ code/experiment3/rrr_savings.py
@@ -2,7 +2,7 @@ import json
 import re
 import matplotlib.pyplot as plt
 import numpy as np
-from math import comb, log
+from math import comb, log, ceil
 
 
 def translate(str):
@@ -20,14 +20,10 @@ def cummulative_dist_f(x, n, p):
     return sum
 
 
-def lower_bound(L, val):
-    index = next(x[0] for x in enumerate(L) if x[1] < val)
-    return index
-
-
-def upper_bound(L, val):
-    index = next(x[0] for x in enumerate(L) if x[1] > val)
-    return index
+def special_ticks(y):
+    ticks = [i for i in range(min(y), max(y), (max(y)-min(y))//5)]
+    ticks_without_close = [i for i in ticks if max(y)-i > 5]
+    return ticks_without_close + [max(y)]
 
 # plt.rcParams["figure.figsize"] = (6,3)
 
@@ -35,20 +31,22 @@ def upper_bound(L, val):
 block_sizes = [31, 63, 127]
 
 fig, axs = plt.subplots(nrows=1, ncols=len(
-    block_sizes), figsize=(9, 3), sharey=True)
+    block_sizes), figsize=(9, 3))
 fig.suptitle(
-    "Ratio of space used by compressed form to raw form as a function of blocks class")
+    "Bits saved by compressed form as a function of blocks class")
 
 for (i, block_size) in enumerate(block_sizes):
-    x = [i for i in range(block_size)]
-    y = [(log(block_size, 2)+log(comb(block_size, i), 2))/log(2**block_size, 2)
-         for i in range(block_size)]
+    x = [i for i in range(block_size+1)]
+    y = [block_size-ceil(log(block_size+1, 2))-ceil(log(comb(block_size, i), 2))
+         for i in range(block_size+1)]
 
-    axs[i].axhline(y=1.0, color='0.9', linestyle='-')
+    print(y)
+    axs[i].axhline(y=0.0, color='0.9', linestyle='-')
     axs[i].plot(x, y, color="k")
+    axs[i].set_yticks(special_ticks(y))
 
-    bad_start = upper_bound(y, 1.0)
-    bad_end = bad_start+lower_bound(y[bad_start+1:], 1.0)
+    bad_start = [i for i in range(len(y)) if y[i] < 0.0][0]
+    bad_end = [i for i in range(len(y)) if y[i] < 0.0][-1]
 
     start, end = axs[i].get_xlim()
     x_ticks = list(range(0, block_size, block_size//4))
@@ -59,14 +57,15 @@ for (i, block_size) in enumerate(block_sizes):
     axs[i].xaxis.set_ticks(x_ticks)
 
     for j in range(len(x_ticks)):
-        if y[x_ticks[j]] > 1.0:
+        if y[x_ticks[j]] < 0.0:
             axs[i].get_xticklabels()[j].set_color("k")
             axs[i].get_xticklabels()[j].set_fontsize("x-large")
         else:
             axs[i].get_xticklabels()[j].set_color("0.5")
     axs[i].set_title(translate("b=" + str(block_size)))
+    #axs[i].set_xlabel("class of block")
 
-
+#axs[0].set_ylabel("bits saved")
 fig.tight_layout()
 plt.show()
 # plt.savefig("binomial_dist.png", bbox_inches='tight')
