075cf28f9b8ba2c8a48a287a9a58267466e3a314
Andrej Korman <andrejkorman@gmail.com>
2022-06-06 01:55:21 +0200

    Hybrid fm-index

--- code/experiment5/visual_sdsl_count.py
+++ code/experiment5/visual_sdsl_count.py
@@ -4,6 +4,15 @@ from os.path import isfile, join
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def translate(s):
+    if s == "ENGLISH":
+        return "Anglický text"
+    elif s == "SOURCES":
+        return "Zdrojové kódy"
+    return s
+
+
 def return_results(path):
     results = {}
     with open(path, "r") as f:
@@ -17,7 +26,8 @@ def return_results(path):
                 used_structure = lines[i+1].split()[3]
                 if "RRR" not in used_structure:
                     continue
-                used_structure_split = used_structure.split("RRR")[1].split("_")
+                used_structure_split = used_structure.split("RRR")[
+                    1].split("_")
                 test_block_size = int(used_structure_split[0])
                 test_cutoff = test_block_size
                 if len(used_structure_split) == 2:
@@ -27,16 +37,21 @@ def return_results(path):
                 test_index_size = 8*int(lines[i + 8].split()[3])
                 test_memory = test_index_size/test_text_size
                 test_time = float(lines[i + 17].split()[3])*1000.0
-                
+
             results[(test_name,
                     test_block_size, test_cutoff)] = (test_time, test_memory)
     return results
 
 
+# DNA
+# SOURCES
+# ENGLISH
+DATASET = "DNA"
+
 path = "./res-count.txt"
 
 colors = ['r', 'g', 'b', 'darkorange']
-block_size_color = {15: 'r', 31: 'g', 63: 'b', 127: 'darkorange' }
+block_size_color = {15: 'r', 31: 'g', 63: 'b', 127: 'darkorange'}
 
 res = return_results(path)
 
@@ -52,17 +67,14 @@ block_sizes = list(block_sizes)
 
 test_cases.sort()
 
-rows, cols = 2, 2
+rows, cols = 1, 1
 
-fig, axs = plt.subplots(rows, cols)
-fig.suptitle('FM-index count for hybrid implementation', fontsize=20)
-
-if rows*cols != len(test_cases):
-    print("Not enough of space in graph.")
-    exit(1)
+fig, axs = plt.subplots(rows, cols, figsize=(8, 6))
+fig.suptitle('FM-index spočítaj pre hybridnú implementáciu', fontsize=20)
 
 for i in range(len(test_cases)):
-    axs[i//cols][i%cols].set_title(test_cases[i], fontsize=16)
+    if test_cases[i] == DATASET:
+        axs.set_title(test_cases[i], fontsize=16)
 
 labels = set()
 
@@ -72,45 +84,45 @@ for file, block_size, cutoff in res.keys():
     time, memory = res[file, block_size, cutoff]
     if cutoff not in allowedCutoffs[block_size]:
         continue
+    if file != DATASET:
+        continue
 
     file_index = test_cases.index(file)
     m = 'o'
-    marker_size = 75
+    marker_size = 140
     l = "orig-" + str(block_size)
     if cutoff != block_size:
         m = "x"
         l = "hybrid-" + str(block_size) + "(" + str(cutoff) + ")"
 
-    row = file_index//rows
-    col = file_index%cols
     color = block_size_color[block_size]
 
     labels.add(l)
-    axs[row][col].scatter([memory], [time], c=color, marker=m, s=marker_size, label=l)
+    axs.scatter([memory], [time], c=color, marker=m, s=marker_size, label=l)
 
-for i in range(rows):
-    axs[i][0].set_ylabel("µs per pattern symbol", fontsize=12)
 
-for i in range(cols):
-    axs[-1][i].set_xlabel("bits per text char", fontsize=12)
+axs.set_ylabel("µs na symbol vzorky", fontsize=16)
+axs.set_xlabel("bity na znak textu", fontsize=16)
 
 labels_handles = {
-  label: handle for ax in fig.axes for handle, label in zip(*ax.get_legend_handles_labels())
+    label: handle for ax in fig.axes for handle, label in zip(*ax.get_legend_handles_labels())
 }
 
 fig.legend(
-  labels_handles.values(),
-  labels_handles.keys(),
-  loc="upper center",
-  bbox_to_anchor=(0.5, 0.05),
-  bbox_transform=plt.gcf().transFigure,
-  ncol=len(list(labels)),
-  handletextpad=0.01,
-  prop={'size': 13}
+    labels_handles.values(),
+    labels_handles.keys(),
+    loc="upper center",
+    bbox_to_anchor=(0.5, 0),
+    bbox_transform=plt.gcf().transFigure,
+    ncol=len(list(labels))//2,
+    handletextpad=0.01,
+    labelspacing=0.0,
+    borderpad=0.1,
+    columnspacing=1.0,
+    prop={'size': 15}
 )
 
 fig.tight_layout(pad=1.0)
-fig.set_size_inches(13, 9)
 
-plt.savefig("vysledky_sdsl_hybrid_count.png")
+plt.savefig(f"vysledky_sdsl_hybrid_count_{DATASET}.png", bbox_inches='tight')
 plt.clf()
