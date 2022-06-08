96d6aec78268458d5e4f22be83b6cc36552c32dc
Andrej Korman <andrejkorman@gmail.com>
2022-06-08 02:25:22 +0200

    Visualize for presentation

--- code/experiment2/visual.py
+++ code/experiment2/visual.py
@@ -9,7 +9,7 @@ def translate(str):
         str = str.replace(preklad[0], preklad[1])
     return str
 
-plt.rcParams["figure.figsize"] = (10,12)
+plt.rcParams["figure.figsize"] = (10,6)
 
 file_paths = ["old_output.txt", "new_output.txt"]
 
@@ -18,6 +18,7 @@ markers = ["o", "x"]
 names = ["sdsl", "my"]
 colors = ['r', 'g', 'b', 'darkorange']
 NUM_OF_QUERIES = 10000.0
+OP = "Rank" # Rank
 
 for file_path in file_paths:
     tests = []
@@ -35,22 +36,24 @@ for file_path in file_paths:
 
 print(results)
 
-fig, axs = plt.subplots(4)
-fig.suptitle('Prístup, rank, select na postupnosti dĺžky 100 000 prvkov (10% jednotiek - 0.47)')
+fig, axs = plt.subplots(1)
+fig.suptitle(f'{OP} na postupnosti dĺžky 100 000 prvkov (10% jednotiek - entropia 0.47)', fontsize=20)
 
 for result_index in range(len(results)):
-    for (index1, access_pattern) in enumerate(["AccessPattern::Random", "AccessPattern::ContinuousRandom"]):
-        for (index2, operation) in enumerate(["Operation::Access", "Operation::Rank"]):
+    for (index1, access_pattern) in enumerate(["AccessPattern::Random"]):
+        for (index2, operation) in enumerate([f"Operation::{OP}"]):
             for (index3, block_size) in enumerate([15, 31, 63, 127]):
                 x = [res[1] for (par, res) in results[result_index] if par[0] == operation and par[1] == access_pattern and int(par[3]) == block_size]
                 y = [res[0]/NUM_OF_QUERIES for (par, res) in results[result_index] if par[0] == operation and par[1] == access_pattern and int(par[3]) == block_size]
-                axs[2*index1+index2].scatter(x, y, label = names[result_index] + "-" + str(block_size), marker = markers[result_index], c = colors[index3])
-            axs[2*index1+index2].legend(loc='center left', bbox_to_anchor=(1, 0.5))
-            axs[2*index1+index2].set_title(translate(operation + " " + access_pattern))
-            axs[2*index1+index2].set_xlabel("bitov/bit")
-            axs[2*index1+index2].set_ylabel("čas (ns)")
+                if block_size == 15 and names[result_index] == "my":
+                    pass
+                else:
+                    axs.scatter(x, y, label = names[result_index] + "-" + str(block_size), marker = markers[result_index], c = colors[index3], s = 120)
+            axs.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+            axs.set_xlabel("bitov/bit", fontsize=16)
+            axs.set_ylabel("čas (ns)", fontsize=16)
 
 fig.tight_layout(pad=3.0)
 
-plt.savefig("vysledky_nase.png", bbox_inches='tight')
+plt.savefig(f"vysledky_nase_{OP}.png", bbox_inches='tight')
 plt.clf()
